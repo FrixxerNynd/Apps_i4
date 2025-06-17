465d9e4ea698ee98f733109a47e9987bd80d6085
FrixxerNynd <julifer2435@gmail.com>
2025-06-16 20:55:18 -0600

    Commit actualizacion proyecto

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,20 @@
-
 import express from "express";
 import morgan from "morgan";
 import authRoutes from "./routes/router.ts";
-import connectDB from "./config/bd.ts";
+import productRoutes from "./routes/product.routes.ts";
+import connectBD from "./config/bd.ts";
+
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
-app.use(morgan("dev"));
-app.use("/api/v1/auth", authRoutes);
+app.use(morgan());
+app.use("api/v1/auth", authRoutes);
+app.use("api/v1/product", productRoutes);
 
-connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log(`Corriendo el servidor en: http://localhost:${PORT}`);
-    });
-});
+connectBD().then(() => {
+	app.listen(PORT, () => {
+		console.log(`Corriendo el servidor en : http://localhost:${PORT}`);
+	});
+});q
--- src/models/Product.ts
+++ src/models/Product.ts
@@ -5,14 +5,21 @@ export interface IProduct extends Document {
   name: string;
   description: string;
   quantity: number;
+  status: boolean;
   price: number;
+  createDate: Date;
+  deleteDate: Date;
 }
 
 const productSchema = new Schema<IProduct>({
   name: { type: String, required: true },
   description: { type: String },
   quantity: { type: Number, required: true },
-  price: { type: Number, required: true }
+  status: { type: Boolean, required: true, default: true },
+  price: { type: Number, required: true },
+  createDate: { type: Date, default: Date.now },
+  deleteDate: { type: Date }
 });
 
 export const Product = model<IProduct>('Product', productSchema, 'product');
+
